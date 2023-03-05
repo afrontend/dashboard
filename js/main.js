515448ea5ecd75ff8c1e52613e22eb68065febdd
afrontend <autoscripts@gmail.com>
2023-03-05 20:52:03 +0900

    Use the event handler to copy button text to the clipboard

--- js/main.js
+++ js/main.js
@@ -1,33 +1,42 @@
-function genBookmark(data) {
-  var mainContainer = document.getElementById("bookmark");
+function genBookmark(data, target) {
   for (var i = 0; i < data.length; i++) {
     if ('name' in data[i]) {
       var a = document.createElement('a');
       a.href = '' + data[i].link
       a.appendChild(document.createTextNode(data[i].name))
-      mainContainer.appendChild(a);
+      target.appendChild(a);
       var s = document.createElement('span');
       s.className = 'description'
       var t = document.createTextNode(' ' + data[i].link)
       s.appendChild(t);
-      mainContainer.appendChild(s);
-      mainContainer.appendChild(document.createElement('br'));
-    }
-    if ('text' in data[i]) {
-      var button = document.createElement('button');
-      button.classList.add('forCopy')
-      button.appendChild(document.createTextNode(data[i].text))
-      mainContainer.appendChild(button);
+      target.appendChild(s);
+      target.appendChild(document.createElement('br'));
     }
   }
 }
 
-/*
- * function toClipboard(text) {
- *   navigator.clipboard.writeText(this.innerText || text)
- *     .then(()=>{})
- * }
- */
+function genTextForCopy(data, target, className) {
+  for (var i = 0; i < data.length; i++) {
+    if ('text' in data[i]) {
+      var button = document.createElement('button');
+      button.classList.add(className)
+      button.appendChild(document.createTextNode(data[i].text))
+      target.appendChild(button);
+    }
+  }
+}
+
+function toClipboard(text) {
+  navigator.clipboard.writeText(this.innerText || text)
+    .then(()=>{})
+}
+
+function installClickHandlerToText(className) {
+    const ary = document.querySelectorAll("." + className);
+    for (let i = 0; i < ary.length; i++) {
+      ary[i].addEventListener("click", toClipboard);
+    }
+}
 
 /*
  * bookmar.json
@@ -43,13 +52,10 @@ fetch('bookmark.json?' + (new Date()).valueOf())
     return response.json();
   })
   .then(function (data) {
-    genBookmark(data);
-    /*
-     * const ary = document.querySelectorAll(".forCopy");
-     * for (let i = 0; i < ary.length; i++) {
-     *   ary[i].addEventListener("click", toClipboard);
-     * }
-     */
+    var target = document.getElementById("bookmark");
+    genBookmark(data, target);
+    genTextForCopy(data, target, 'forCopy');
+    installClickHandlerToText('forCopy')
   })
   .catch(function (err) {
     console.log('error: ' + err);
