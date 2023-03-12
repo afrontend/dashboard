a569bc75c61f312baec9b1ce68e01ad03afe3984
afrontend <autoscripts@gmail.com>
2023-03-13 00:44:18 +0900

    Show message when copied text

--- src/TextForCopy.js
+++ src/TextForCopy.js
@@ -1,28 +1,40 @@
 import React, {useEffect, useState} from "react";
 import '../css/button.css'
 
+const COPIED_MESSAGE = 'copied'
+
 export function TextForCopy() {
-  const [textAry, setText] = useState([])
+  const [textAry, setTextAry] = useState([])
+  const [message, setMessage] = useState([])
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch('json/text.json?');
       const tList = await response.json();
-      setText(tList)
+      setTextAry(tList)
     };
     fetchData();
   }, []);
 
-  function toClipboard(e) {
-    const text = e.target.innerText
+
+  function toClipboard(text) {
     navigator.clipboard.writeText(text)
-      .then(()=>{})
+      .then(()=>{
+        setMessage(text)
+        setTimeout(() => {
+          setMessage('')
+        }, 2000);
+      })
   }
 
   if (textAry.length === 0) return 'Loading...'
 
   return textAry.map(
-    (text, index) => <button key={text.content + index} onClick={toClipboard}>{text.content}</button>
+    (text, index) => <div>
+      <button key={text.content + index} onClick={() => toClipboard(text.content)}>{text.content}</button>
+      &nbsp;
+      {message === text.content && COPIED_MESSAGE}
+    </div>
   )
 }
 
