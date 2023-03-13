ae70efb06f83ea5be0552ef2e66f04d752ca6125
afrontend <autoscripts@gmail.com>
2023-03-13 23:06:21 +0900

    Use onChange instead of onClick for checked

--- src/Switch.js
+++ src/Switch.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export function Switch({name = '', onOff, setOnOff}) {
   return <div>
-    <input type="checkbox" id="cb" checked={onOff} onClick={e => setOnOff(e.target.checked)} />
-    <label for="cb">{name}</label>
+    <input type="checkbox" id="cb" checked={onOff} onChange={e => setOnOff(e.target.checked)} />
+    <label htmlFor="cb">{name}</label>
   </div>
 }
 
