6e4b91e595df4ea41d6c861ead7bf07d2a8f9bf5
afrontend <autoscripts@gmail.com>
2023-03-13 23:06:00 +0900

    Update background color

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React, {useState} from "react";
 import { Bookmark } from "./Bookmark";
 import { TextForCopy } from "./TextForCopy";
 import { Switch } from "./Switch";
+import '../css/App.css'
 
 export function App() {
   const [hasDescription, toggleDescription] = useState(false)
-  return <div>
+  return <div className="background">
     <h1>Dashboard</h1>
     <Bookmark hasDescription={hasDescription} />
     <Switch onOff={hasDescription} setOnOff={toggleDescription} name="show description"/>
