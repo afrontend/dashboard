140310285ce5c5ffc839b428a5b7cd6dded3bba9
afrontend <autoscripts@gmail.com>
2023-03-26 10:49:40 +0900

    Show an error message when failing to load the JSON file

--- components/Bookmark.js
+++ components/Bookmark.js
@@ -4,18 +4,28 @@ import buttonCss from '../css/button.module.css';
 
 export function Bookmark({hasDescription, jsonFilename = 'bookmark.json'}) {
   const [bookmarkAry, setBookmark] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [errorMsg, setErrorMsg] = useState("")
 
   useEffect(() => {
     const fetchData = async () => {
       const path = (`json/${jsonFilename}?` + (new Date()).valueOf())
-      const response = await fetch(path);
-      const bList = await response.json();
-      setBookmark(bList)
+      try {
+        const response = await fetch(path);
+        const bList = await response.json();
+        setBookmark(bList)
+      } catch(error) {
+        console.log(error)
+        setErrorMsg(String(error))
+      } finally {
+        setLoading(false)
+      }
     };
     fetchData();
   }, []);
 
-  if (bookmarkAry.length === 0) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>
+  if (errorMsg) return <pre>{errorMsg}</pre>
 
   return bookmarkAry.map(
     (b, index)  => <div style={{marginBottom: '0.5rem'}} key={b.link + index}>
