d85f44f17d7febbe8e01bf78011c7707545dcfd6
afrontend <autoscripts@gmail.com>
2023-05-18 19:43:25 +0900

    Typo

--- components/Switch.js
+++ components/Switch.js
@@ -3,7 +3,7 @@ import React from "react";
 export function Switch({name = '', onOff, setOnOff}) {
   return <div>
     <input type="checkbox" id="cb" checked={onOff} onChange={e => setOnOff(e.target.checked)} />
-    <label htmlFor="cb"> {name}</label>
+    <label htmlFor="cb">{name}</label>
   </div>
 }
 
