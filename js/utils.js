5a0f6552fda3722acf24795f675255216b968096
afrontend <autoscripts@gmail.com>
2025-07-02 21:38:59 +0900

    Refactor, Remove arg of getJsonData

--- js/utils.js
+++ js/utils.js
@@ -5,7 +5,8 @@ const initialData = [
   },
 ];
 
-export function getJsonData(queryString) {
+export function getJsonData() {
+  const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const data = urlParams.get("data");
   if (data) {
