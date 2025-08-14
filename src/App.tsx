842ced84b1b045847482f6101461a811ebde87f3
afrontend <autoscripts@gmail.com>
2025-08-14 23:52:18 +0900

    Update title style

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ export function App() {
 
   return (
     <div className="bg-gray-300 mx-auto max-w-6xl p-5">
-      <h1>Dashboard</h1>
+      <h1 className="text-4xl font-bold text-center text-gray-800 mb-6 pb-4 border-b-2 border-gray-400">Dashboard</h1>
       <div className="mb-4 ml-4">
         <SwitchComponent />
       </div>
