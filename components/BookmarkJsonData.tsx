b0dae863a6ef55eed41718e49687b6cc6bf5b87f
afrontend <autoscripts@gmail.com>
2025-09-09 20:22:26 +0900

    Make bookmarkAry required and improve key generation

--- components/BookmarkJsonData.tsx
+++ components/BookmarkJsonData.tsx
@@ -3,13 +3,13 @@ import React from "react";
 import { TBookmark } from "../types";
 
 interface BookmarkJsonDataProps {
-  bookmarkAry?: TBookmark[];
+  bookmarkAry: TBookmark[];
 }
 
 export function BookmarkJsonData({ bookmarkAry = [] }: BookmarkJsonDataProps) {
   const bAry = bookmarkAry && bookmarkAry.length > 0 ? bookmarkAry : [];
   return bAry.map((b, index) => (
-    <div style={{ marginBottom: "0.5rem" }} key={b[1] + index}>
+    <div style={{ marginBottom: "0.5rem" }} key={`${b[1]}-${index}`}>
       <a href={b[1]}>{b[0]}</a>
     </div>
   ));
